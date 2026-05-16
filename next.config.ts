0203009797724b825615dd1333f4f2c03de809eb
handsomelad2011-web <handsomelad2011@gmail.com>
2026-05-16 17:42:46 +0530

    fixing av

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_YOUTUBE_API_KEY: process.env.NEXT_PUBLIC_YOUTUBE_API_KEY,
+  },
+};
 
 export default nextConfig;
